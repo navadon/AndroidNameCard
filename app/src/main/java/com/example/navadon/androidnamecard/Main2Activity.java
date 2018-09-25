d89617596ff8907dd48d8f3b9ba4286ab6ad2e4e
mayniramol2540 <niramol.i3gateway@gmail.com>
2018-09-26 01:17:24 +0700

    remove bar,blink

---
+++ app/src/main/java/com/example/navadon/androidnamecard/Main2Activity.java
@@ -0,0 +1,27 @@
+package com.example.navadon.androidnamecard;
+
+import android.animation.ObjectAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+
+public class Main2Activity extends AppCompatActivity {
+    int onStartCount = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        if (savedInstanceState == null) // 1st time
+        {
+            this.overridePendingTransition(R.anim.blink,
+                    R.anim.blink);
+        } else // already created so reverse animation
+        {
+            onStartCount = 2;
+        }
+    }
+
+
+
+}
