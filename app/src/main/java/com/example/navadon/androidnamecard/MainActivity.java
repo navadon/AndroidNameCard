ba6d9e6169d810bdc98216d4529a262999549b98
smmile18 <spch.smmile@gmail.com>
2018-09-27 17:04:33 +0700

    add onclick function

--- app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.navadon.androidnamecard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View.OnClickListener onClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindView();
+        initView();
     }
+    private void bindView(){
+
+    }
+
+    private void initView(){
+        initOnClickListener();
+        findViewById(R.id.btn_click).setOnClickListener(onClickListener);
+
+    }
+
+    // You don't have to bind any functions to "android:onClick" in layout XML file.
+    private void initOnClickListener(){
+        onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_click :
+                        Intent intent = new Intent( MainActivity.this,Main2Activity.class);
+                        startActivity(intent);
+                        break;
+
+                }
+
+            }
+        };
+    }
+
+
+
+
+
+
+
 }
