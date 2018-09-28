f830b609c04f1634bd8b84f3a6d62da8b3354d52
Tanapon Nanta <pangpond556_@hotmail.com>
2018-09-28 11:45:15 +0700

    Add onclicklistener

--- app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.navadon.androidnamecard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View.OnClickListener onClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initOnClickListener() {
+        onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btn_swap:
+                        // do something
+                        break;
+                }
+            }
+        };
+    }
+
+    private void initView() {
+        initOnClickListener();
+        findViewById(R.id.btn_swap).setOnClickListener(onClickListener);
     }
 }
