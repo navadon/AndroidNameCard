35d5b4fe0263ae0290601b227c6d7ff52423833f
nattawut0641 <nattawut.khamchai@gmail.com>
2018-09-28 22:29:40 +0700

    เสร็จแต่ยังไม่สวย

--- app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.navadon.androidnamecard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View.OnClickListener onClickListener;
+    CardView cv_showData;
+    boolean check =false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindView();
+        initView();
     }
+
+    private void bindView() {
+        cv_showData = findViewById(R.id.cv_All);
+    }
+
+    private void initView() {
+
+        initOnclickListener();
+        findViewById(R.id.btn_showData).setOnClickListener(onClickListener);
+
+    }
+
+    private void initOnclickListener() {
+        onClickListener = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.btn_showData:
+                        checkButton();
+                        break;
+
+                }
+
+            }
+        };
+    }
+
+    private void checkButton() {
+        if(check == false){
+            cv_showData.setVisibility(View.VISIBLE);
+            check = true;
+        }
+        else if (check == true){
+            cv_showData.setVisibility(View.INVISIBLE);
+            check = false;
+        }
+    }
+
 }
