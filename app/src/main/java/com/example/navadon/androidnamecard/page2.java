b7ab78d70916d873c4df271fa73135f9716df6fe
magmanex <timecrisis1995@live.com>
2018-09-29 23:44:22 +0700

    Add card name 2 layout and get style

--- app/src/main/java/com/example/navadon/androidnamecard/page2.java
+++ app/src/main/java/com/example/navadon/androidnamecard/page2.java
@@ -1,19 +1,19 @@
 package com.example.navadon.androidnamecard;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class page2 extends AppCompatActivity {
     private ImageView img;
     private View.OnClickListener onClickListener;
     private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.page2);
         img = findViewById(R.id.profile_image);
         initOnClickListener();
         findViewById(R.id.profile_image).setOnClickListener(onClickListener);
@@ -23,9 +23,8 @@ public class MainActivity extends AppCompatActivity {
         onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(v.getId() == R.id.profile_image) {
-                    intent = new Intent(MainActivity.this,page2.class);
-                    startActivity(intent);}
+                intent = new Intent(page2.this,MainActivity.class);
+                startActivity(intent);
             }
         };
     }
