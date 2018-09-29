8e82c4e0004839e64ef87cef635e256efb2b68ae
Chaloempong Budkrajang <chaloempong67@gmail.com>
2018-09-30 00:44:40 +0700

    Add Information to NameCard with xml and Use Option 4 for OnClickEvent

--- app/src/main/java/com/example/navadon/androidnamecard/InformationActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/InformationActivity.java
@@ -3,22 +3,27 @@ package com.example.navadon.androidnamecard;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class InformationActivity extends AppCompatActivity {
 
     private View.OnClickListener onClickListener;
+    private TextView link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_information);
         initView();
     }
 
     private void initView(){
         initOnClickListener();
-        findViewById(R.id.btnInformation).setOnClickListener(onClickListener);
+        findViewById(R.id.btnMain).setOnClickListener(onClickListener);
+        link = findViewById(R.id.websiteView);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     private void initOnClickListener() {
@@ -26,15 +31,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
-                    case R.id.btnInformation:
-                        goToInformation();
+                    case R.id.btnMain:
+                        goToMain();
                         break;
                 }
             }
         };
     }
 
-    private void goToInformation() {
-        startActivity(new Intent(this,InformationActivity.class));
+    private void goToMain() {
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
