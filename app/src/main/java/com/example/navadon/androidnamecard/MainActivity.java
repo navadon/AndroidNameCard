265d2c912d81691a76a3b5128629088e5e3c8bff
bombubble <bombubble@gmail.com>
2018-10-01 21:29:57 +0700

    Assignment 4

--- app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.navadon.androidnamecard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button change;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.text);
+        tv.setText("Supapitch Thungtragul\nFaculty of Engineering, CMU");
+        change = (Button) findViewById(R.id.change);
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onChange();
+            }
+        });
+    }
+
+    public void onChange(){
+        tv = findViewById(R.id.text);
+        tv.setText("Supapitch Thungtragul\n580611061");
     }
 }
