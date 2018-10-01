0bd11a9e55cf64d959433908eef448cb49606027
cwm003 <warakornchamp@gmail.com>
2018-10-01 23:10:31 +0700

    info page languages switch

--- app/src/main/java/com/example/navadon/androidnamecard/InfoActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/InfoActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoActivity extends Activity implements View.OnClickListener {
 
@@ -12,11 +14,21 @@ public class InfoActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button1).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        finish();
+        if(((Button)v).getText().equals(getString(R.string.buttonLang))){
+            ((Button)findViewById(R.id.button1)).setText(R.string.buttonLangTH);
+            ((TextView)findViewById(R.id.textViewNum0)).setText(R.string.phoneNumberTitleTH);
+            ((TextView)findViewById(R.id.textViewAddr0)).setText(R.string.addressTitleTH);
+            ((TextView)findViewById(R.id.textViewAddr1)).setText(R.string.addressTH);
+        }else{
+            ((Button)findViewById(R.id.button1)).setText(R.string.buttonLang);
+            ((TextView)findViewById(R.id.textViewNum0)).setText(R.string.phoneNumberTitle);
+            ((TextView)findViewById(R.id.textViewAddr0)).setText(R.string.addressTitle);
+            ((TextView)findViewById(R.id.textViewAddr1)).setText(R.string.address);
+        }
     }
 }
--- app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener{
+
+//    View inflatedView = getLayoutInflater().inflate(R.layout.activity_info, null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
