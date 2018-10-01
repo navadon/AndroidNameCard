5fb22154ea9995ad741f65f405b4acdc6bc46b71
Alicis <natthapong_chan@cmu.ac.th>
2018-10-01 15:01:38 +0700

    Add solutions

---
+++ app/src/main/java/com/example/navadon/androidnamecard/InfoActivity.java
@@ -0,0 +1,17 @@
+package com.example.navadon.androidnamecard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+    }
+
+}
--- app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.navadon.androidnamecard;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button about_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindView();
+    }
+
+    private void bindView() {
+       about_button = findViewById(R.id.btn_next);
+    }
+
+    public void process(View v) {
+        if (v == about_button) {
+            next();
+        }
+        hideKeyboardInput(v);
+    }
+
+    public void next() {
+        Intent i = new Intent(MainActivity.this, InfoActivity.class);
+        MainActivity.this.startActivity(i);
+    }
+
+    private void hideKeyboardInput(View v){
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 }
