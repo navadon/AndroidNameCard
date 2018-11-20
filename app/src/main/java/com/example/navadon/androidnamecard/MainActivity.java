ed8bb8f4c252bfa02cd6003aef3f2fbb56742e10
Thanongsak <thanongsak_n@cmu.ac.th>
2018-11-20 17:19:36 +0700

    swap data

--- app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.navadon.androidnamecard;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.navadon.androidnamecard.databinding.ActivityMainBinding;
 
@@ -19,28 +21,46 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
 
-    private void initOnClickListener() {
-        onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.btn_swap:
-                        break;
-                }
-            }
-        };
-    }
-
     private void initView() {
         viewmodel = new MainViewModel(getApplicationContext());
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setViewmodel(viewmodel);
+        bind(binding);
+        Button btn_swap = (Button) findViewById(R.id.btn_swap);
+        btn_swap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapData();
+                bind(binding);
+            }
+        });
+
+    }
+
+    private void bind(ActivityMainBinding binding ){
         binding.tvName.setText(viewmodel.getName());
         binding.tvSurname.setText(viewmodel.getSurname());
         binding.tvPosition.setText(viewmodel.getPosition());
         binding.tvEmail.setText(viewmodel.getEmail());
         binding.tvPhone.setText(viewmodel.getPhoneNumber());
-        initOnClickListener();
-        findViewById(R.id.btn_swap).setOnClickListener(onClickListener);
+        binding.imgProfile.setImageResource(viewmodel.getProfile());
+    }
+
+    private void swapData(){
+        if(viewmodel.getName().equals(getResources().getString(R.string.name_tim))){
+            viewmodel.setName(getResources().getString(R.string.name_pond));
+            viewmodel.setSurname(getResources().getString(R.string.surName_pond));
+            viewmodel.setPosition(getResources().getString(R.string.position_pond));
+            viewmodel.setEmail(getResources().getString(R.string.email_pond));
+            viewmodel.setPhone_number(getResources().getString(R.string.phone_pond));
+            viewmodel.setProfile(R.drawable.profile_pond);
+        } else {
+            viewmodel.setName(getResources().getString(R.string.name_tim));
+            viewmodel.setSurname(getResources().getString(R.string.surName_tim));
+            viewmodel.setPosition(getResources().getString(R.string.position_tim));
+            viewmodel.setEmail(getResources().getString(R.string.email_tim));
+            viewmodel.setPhone_number(getResources().getString(R.string.phone_tim));
+            viewmodel.setProfile(R.drawable.profile_tim);
+        }
     }
 }
--- app/src/main/java/com/example/navadon/androidnamecard/MainViewModel.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainViewModel.java
@@ -18,7 +18,7 @@ public class MainViewModel extends ViewModel {
     private String position;
     private String email;
     private String phone_number;
-    private ImageView profile_picture;
+    private Integer profile;
     private Context context;
 
     public MainViewModel(Context context) {
@@ -28,16 +28,7 @@ public class MainViewModel extends ViewModel {
         this.position = context.getResources().getString(R.string.position_tim);
         this.email = context.getResources().getString(R.string.email_tim);
         this.phone_number = context.getResources().getString(R.string.phone_tim);
-        int img = R.drawable.profile_tim;
-        this.profile_picture.setImageResource(img);
-    }
-
-    public void Switch() {
-        this.name = context.getResources().getString(R.string.name_pond);
-        this.surname = context.getResources().getString(R.string.surName_pond);
-        this.position = context.getResources().getString(R.string.position_pond);
-        this.email = context.getResources().getString(R.string.email_pond);
-        this.phone_number = context.getResources().getString(R.string.phone_pond);
+        this.profile = R.drawable.profile_tim;
     }
 
     public String getName() { return this.name; }
@@ -45,5 +36,12 @@ public class MainViewModel extends ViewModel {
     public String getPosition() { return this.position; }
     public String getEmail() { return this.email; }
     public String getPhoneNumber() { return this.phone_number; }
-    public ImageView getProfilePicture(View view) { return (ImageView) view.findViewById(R.id.img_profile); }
+    public Integer getProfile() { return this.profile; }
+
+    public void setName(String name) { this.name = name; }
+    public void setSurname(String surname) { this.surname = surname; }
+    public void setPosition(String position) { this.position = position; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPhone_number(String phone_number) { this.phone_number = phone_number; }
+    public void setProfile(Integer profile) { this.profile = profile; }
 }
