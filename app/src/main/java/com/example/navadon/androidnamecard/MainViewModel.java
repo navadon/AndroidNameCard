b3f7ea3053042ec0409a3fe46665af8dcc05027b
Supakarn <supakarn.chaidaroon@gmail.com>
2018-11-25 23:57:57 +0700

    Finished V.3

--- app/src/main/java/com/example/navadon/androidnamecard/MainViewModel.java
+++ app/src/main/java/com/example/navadon/androidnamecard/MainViewModel.java
@@ -24,10 +24,6 @@ public class MainViewModel extends ViewModel {
         this.myname = "Supakarn Chaidaroon";
         this.subname = "4th year student";
         this.address = "4th Floor, 30 Years Building\nFaculty of Engineering, Chiang Mai University\nEmail: supakarn_chaidaroon@cmu.ac.th";
-        this.depnameth = "ภาควิชาวิศวกรรมคอมพิวเตอร์";
-        this.mynameth = "ศุภการ  ชัยดรุณ";
-        this.subnameth = "นักศึกษาชั้นปีที่ 4";
-        this.addressth = "ชั้น 4 อาคาร 30 ปี\nคณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่\nอีเมล: supakarn_chaidaroon@cmu.ac.th";
     }
 
     public String getDepname() {
@@ -54,51 +50,24 @@ public class MainViewModel extends ViewModel {
         return en;
     }
 
-    public String getDepnameth() {
-        return depnameth;
-    }
-
-    public String getMynameth() {
-        return mynameth;
-    }
-
-    public String getSubnameth() {
-        return subnameth;
-    }
-
-    public String getAddressth() {
-        return addressth;
-    }
 
     public void setAddress(String address) {
         this.address = address;
     }
 
-    public void setAddressth(String addressth) {
-        this.addressth = addressth;
-    }
 
     public void setDepname(String depname) {
         this.depname = depname;
     }
 
-    public void setDepnameth(String depnameth) {
-        this.depnameth = depnameth;
-    }
 
     public void setMyname(String myname) {
         this.myname = myname;
     }
 
-    public void setMynameth(String mynameth) {
-        this.mynameth = mynameth;
-    }
 
     public void setSubname(String subname) {
         this.subname = subname;
     }
 
-    public void setSubnameth(String subnameth) {
-        this.subnameth = subnameth;
-    }
 }
