bc252c41dcbd41564c3f5043a211a85ffee9e0ca
Chaloempong Budkrajang <chaloempong67@gmail.com>
2018-12-03 09:44:24 +0700

    Edit onActivityResult for firebase

--- app/src/main/java/com/example/navadon/androidnamecard/signin/SignInActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/signin/SignInActivity.java
@@ -73,12 +73,18 @@ public class SignInActivity extends AppCompatActivity implements
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
         }
     }
 
